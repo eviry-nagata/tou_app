848dcf1b72cb69b3f0cd55400c7efbeb41236af6
eviry-nagata <knagata@eviry.com>
2024-08-26 23:02:23 +0900

    修正

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,7 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+# ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+# require 'bundler/setup' # Set up gems listed in the Gemfile.
+# require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
 # require 'bootsnap'
 # env = ENV['RAILS_ENV'] || "development"
 # Bootsnap.setup(
